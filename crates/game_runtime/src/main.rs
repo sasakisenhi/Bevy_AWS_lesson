3cc1d3a42e2196229e16ef42e2c7b8abce7dc2cb
sasakisenhi <33shin12@gmail.com>
2026-01-02 11:33:38 +0900

    CI調整

--- crates/game_runtime/src/main.rs
+++ crates/game_runtime/src/main.rs
@@ -1,34 +1,35 @@
 use bevy::prelude::*;
-use game_core::Player;
 use bevy::input::ButtonInput;
-use game_logic::{Command,apply_command};
+
+use game_core::Player;
+use game_logic::{Command, apply_command};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update,sync_player_position)
         .add_systems(Update, handle_input)
+        .add_systems(Update, sync_player_position)
         .run();
 }
+
 #[derive(Component)]
-pub struct PlayerComponent {
-    pub player: Player,
+struct PlayerComponent {
+    player: Player,
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn((
-        Camera2d,
-        Transform::from_xyz(0.0, 0.0, 1000.0),
-    ));
+    // 2D カメラ（これだけでよい）
+    commands.spawn(Camera2d);
 
+    // 四角いスプライト
     commands.spawn((
         Sprite {
             color: Color::srgb(0.2, 0.7, 0.9),
             custom_size: Some(Vec2::new(100.0, 100.0)),
             ..default()
         },
-       Transform::from_xyz(0.0, 0.0, 0.0),
+        Transform::from_xyz(0.0, 0.0, 0.0),
         PlayerComponent {
             player: Player {
                 position: game_core::Vec2 { x: 0.0, y: 0.0 },
@@ -38,20 +39,20 @@ fn setup(mut commands: Commands) {
 }
 
 fn handle_input(
-    keybord_input: Res<ButtonInput<KeyCode>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<&mut PlayerComponent>,
 ) {
-    for mut pc in query.iter_mut() {
-        if keybord_input.just_pressed(KeyCode::KeyW) {
+    for mut pc in &mut query {
+        if keyboard_input.just_pressed(KeyCode::KeyW) {
             apply_command(&mut pc.player, Command::MoveUp);
         }
-        if keybord_input.just_pressed(KeyCode::KeyS) {
+        if keyboard_input.just_pressed(KeyCode::KeyS) {
             apply_command(&mut pc.player, Command::MoveDown);
         }
-        if keybord_input.just_pressed(KeyCode::KeyA) {
+        if keyboard_input.just_pressed(KeyCode::KeyA) {
             apply_command(&mut pc.player, Command::MoveLeft);
         }
-        if keybord_input.just_pressed(KeyCode::KeyD) {
+        if keyboard_input.just_pressed(KeyCode::KeyD) {
             apply_command(&mut pc.player, Command::MoveRight);
         }
     }
@@ -59,9 +60,9 @@ fn handle_input(
 
 fn sync_player_position(
     mut query: Query<(&PlayerComponent, &mut Transform)>,
-    ) {
-        for (player, mut transform) in query.iter_mut() {
-            transform.translation.x = player.player.position.x;
-            transform.translation.y = player.player.position.y;
-        }
+) {
+    for (player, mut transform) in &mut query {
+        transform.translation.x = player.player.position.x;
+        transform.translation.y = player.player.position.y;
     }
+}
